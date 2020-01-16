7d7c9800ccec478878248fa078ca599152d23aaa
Sebastiaan van Steenis <mail@superseb.nl>
2020-01-16 13:56:06 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/superseb/go-helloworld
+
+go 1.13
+
+require github.com/superseb/go-libs v0.0.0-20200116124112-fd98dc6d0873
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	seb "github.com/superseb/go-libs"
+)
+
+func main() {
+	string := seb.EpochString("yihaa")
+	fmt.Println(string)
+}
