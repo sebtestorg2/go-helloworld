34a8e188ccfbd3c3b0cf36915d5db2af0db27b9c
Sebastiaan van Steenis <superseb@users.noreply.github.com>
2021-11-28 10:43:33 +0100

    testing

--- main.go
+++ main.go
@@ -8,4 +8,9 @@ import (
 func main() {
 	string := seb.EpochString("yihaa")
 	fmt.Println(string)
+	// Yayayaya
+	string2 := seb.EpochString("yihaa2")
+	fmt.Println(string2)
+	// Yayayaya
+	// etc...
 }
